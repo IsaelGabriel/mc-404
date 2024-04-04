c0eca5e82e2abd5b50fe3aabb70d2dde4cfc951d
IsaelGabriel <isaelgsfaria@gmail.com>
2024-04-04 19:49:34 -0300

    Add base input

---
+++ lab-04/main.c
@@ -0,0 +1,105 @@
+int read(int __fd, const void *__buf, int __n){
+    int ret_val;
+  __asm__ __volatile__(
+    "mv a0, %1           # file descriptor\n"
+    "mv a1, %2           # buffer \n"
+    "mv a2, %3           # size \n"
+    "li a7, 63           # syscall write code (63) \n"
+    "ecall               # invoke syscall \n"
+    "mv %0, a0           # move return value to ret_val\n"
+    : "=r"(ret_val)  // Output list
+    : "r"(__fd), "r"(__buf), "r"(__n)    // Input list
+    : "a0", "a1", "a2", "a7"
+  );
+  return ret_val;
+}
+
+void write(int __fd, const void *__buf, int __n)
+{
+  __asm__ __volatile__(
+    "mv a0, %0           # file descriptor\n"
+    "mv a1, %1           # buffer \n"
+    "mv a2, %2           # size \n"
+    "li a7, 64           # syscall write (64) \n"
+    "ecall"
+    :   // Output list
+    :"r"(__fd), "r"(__buf), "r"(__n)    // Input list
+    : "a0", "a1", "a2", "a7"
+  );
+}
+
+void exit(int code)
+{
+  __asm__ __volatile__(
+    "mv a0, %0           # return code\n"
+    "li a7, 93           # syscall exit (64) \n"
+    "ecall"
+    :   // Output list
+    :"r"(code)    // Input list
+    : "a0", "a7"
+  );
+}
+
+void _start()
+{
+  int ret_code = main();
+  exit(ret_code);
+}
+
+#define STDIN_FD  0
+#define STDOUT_FD 1
+#define BUFFER_SIZE 20
+
+int pow(int a, int b) {
+    int c = 1;
+    for(int i = 0; i < b; i++) {
+        c *= a;
+    }
+    return c;
+}
+
+void decimal_conversion(int* conv, char* str) {
+    int p = 0;
+    int i = BUFFER_SIZE - 1;
+    while(i > 0) {
+        if(*(str + i) >= 48 && *(str + i) <= 57) {
+            *conv += (*(str + i) - 48) * pow(10, p);
+            p++;
+        }
+        i--;
+    }
+    if(*str == 45) *conv *= -1;
+}
+
+void hex_conversion(int* conv, char* str) {
+    int p = 0;
+    int i = BUFFER_SIZE - 1;
+    while(i > 0) {
+        if(*(str + i) >= 48 && *(str + i) <= 57) {
+            *conv += (*(str + i) - 48) * pow(16, p);
+            p++;
+        }else if (*(str + i) >= 65 && *(str + i) <= 70){
+            *conv += (*(str + i) - 55) * pow(16, p);
+            p++;
+        }
+        i--;
+    }
+}
+
+int main()
+{
+    char str[20];
+    /* Read up to 20 bytes from the standard input into the str buffer */
+    int n = read(STDIN_FD, str, 20);
+    int conv = 0;
+
+    if(str[0] == '0' && str[1] == 'x') {
+        hex_conversion(&conv, str);
+    }else {
+        decimal_conversion(&conv, str);
+    }
+
+    /* Write n bytes from the str buffer to the standard output */
+    write(STDOUT_FD, str, n);
+    return 0;
+}
