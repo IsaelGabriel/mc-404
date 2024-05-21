8756a6e8e8db8f1955e060cc07dcbd9b6a05eec2
IsaelGabriel <isaelgabrielfaria@gmail.com>
2024-05-21 04:26:24 -0300

    Attempt at fix

--- lab-08/arquivo.s
+++ lab-08/arquivo.s
@@ -12,20 +12,17 @@ main:
     jal open            # call open()
     la a1, fd           # a1 = fd
     sw a0, 0(a1)        # *fd = open()
-    li a0, 3            # a0 = 0
+    li a0, 3            # a0 = 3
     jal skip_chars      # skip first 3 chars
     jal read_ascii      # a0 = read_ascii()
     la s0, size         # s0 = size
     sh a0, 0(s0)        # size[0] = a0
-    jal read            # skip 1 char
     jal read_ascii      # a0 = read_ascii()
     sh a0, 2(s0)        # size[1] = a0
     mv a1, a0           # a1 = height
     lh a0, 0(s0)        # a0 = width
     jal set_canvas_size # set_canvas_size(a0, a1)
-    jal read            # skip 1 char
     jal read_ascii      # skip next number
-    jal read            # skip 1 char
     jal draw_image      # call draw_image()
     lw ra, 0(sp)        # retrieve return address
     addi sp, sp, 4      # free 4 bytes
@@ -36,13 +33,11 @@ draw_image:                                     # void draw_image()
     sw ra, 0(sp)                                # store ra
     li a1, 0                                    # y = 0
     for_y_in_draw_image:
-        la t0, size                             # t0 = &size
-        lh t0, 2(t0)                            # t0 = height
+        lh t0, 2(s0)                            # t0 = height
         bge a1, t0, end_draw_image              # if y >= height: end y loop
         li a0, 0                                # x = 0
         for_x_in_draw_image:
-            la t0, size                         # t0 = &size
-            lh t0, 0(t0)                        # t0 = width
+            lh t0, 0(s0)                        # t0 = width
             bge a0, t0, end_for_x_in_draw_image # if x >= width: end x loop
             sh a0, 4(sp)                        # store x
             sh a1, 6(sp)                        # store y
@@ -53,7 +48,6 @@ draw_image:                                     # void draw_image()
             lb a2, 0(t0)                        # a2 = current_char
             addi a2, a2, -'0'                   # a2 -= '0'
             jal set_pixel                       # call set_pixel(x, y, current_char)
-            jal read                            # skip 1 char
             lh a0, 4(sp)                        # retrieve x
             lh a1, 6(sp)                        # retrieve y
             addi a0, a0, 1                      # x++
@@ -75,13 +69,12 @@ read_ascii:                         # int read_ascii()
     while_read_ascii:
         sw a0, 4(sp)                # sp[1] = n
         jal read                    # call read()
-        la t0, current_char         # t0 = &current_char
         lw a0, 4(sp)                # a0 = n
+        la t0, current_char         # t0 = &current_char
         lb a1, 0(t0)                # a1 = current_char
         addi a1, a1, -'0'           # a1 -= '0'
-        li t0, 9                    # t0 = 9
-        bgtu a1, t0, end_read_ascii # if current_char > 9: end loop
         li t0, 10                   # t0 = 10
+        bgeu a1, t0, end_read_ascii # if current_char >= 10: end loop
         mul a0, a0, t0              # n *= 10
         add a0, a0, a1              # n += current_char
         j while_read_ascii          # loop
@@ -94,10 +87,12 @@ read_ascii:                         # int read_ascii()
 
 
 set_pixel:            # void set_pixel(a0: uint x, a1: uint y, a2: byte color)
-    slli t0, a2, 8    # t0 = color << 8
-    add a2, a2, t0    # a2 = color + (color << 8)
-    slli t0, t0, 8    # t0 <<= 8
-    add a2, a2, t0    # a2 = [color, color, color]
+    andi a2, a2, 0xFF # 0xFF masks a2
+    mv t0, a2
+    slli a2, a2, 8
+    add a2, a2, t0
+    slli a2, a2, 8
+    add a2, a2, t0
     slli a2, a2, 8    # a2 = [color, color, color, 0x0]
     addi a2, a2, 0xFF # a2 = [color, color, color, 0xFF]
     li a7, 2200       # syscall setPixel
@@ -106,6 +101,7 @@ set_pixel:            # void set_pixel(a0: uint x, a1: uint y, a2: byte color)
 
 set_canvas_size: # void set_canvas_size(a0: uint width, a1: uint height)
     li a7, 2201  # syscall setCanvasSize
+    ecall
     ret
 
 skip_chars:                         # void skip_chars(a0: n)
